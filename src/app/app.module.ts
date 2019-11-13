7c99fb23c2257ab026e2ba06c73b1487e0448c14
avijitmaharana <avijitmaharana@outlook.com>
2019-11-13 20:54:48 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatProgressBarModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
